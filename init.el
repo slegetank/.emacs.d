6b9b03b0fc5559ba04bcdb0c3c6a5a71bf3d7f97
Shuai Zhao <shuai.zhao@wiimu.com>
2017-06-27 12:11:20 +0800

    too long to commit

--- init.el
+++ init.el
@@ -12,7 +12,9 @@
 (org-babel-load-file (expand-file-name "init/init-blog.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init/init-elisp.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init/init-python.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "init/init-js.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init/init-dired.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "init/init-mu4e.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init/init-help.org" user-emacs-directory))
 
 ;;(mapc 'org-babel-load-file (directory-files (expand-file-name "init" user-emacs-directory) t "\\.org$"))
