8e7595ca325a88a53fe87ccebdd911712a14c686
Shuai Zhao <shuai.zhao@linkplay.com>
2017-10-23 10:08:09 +0800

    It's a long time; commit.

--- init.el
+++ init.el
@@ -44,6 +44,7 @@
 (org-babel-load-file (expand-file-name "init/init-mu4e.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init/init-git.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init/init-project.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "init/init-tools.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init/init-help.org" user-emacs-directory))
 
 ;;(mapc 'org-babel-load-file (directory-files (expand-file-name "init" user-emacs-directory) t "\\.org$"))
